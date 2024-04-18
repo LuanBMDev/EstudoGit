1530cc0a6dcff7dabfc7a2ef6a3e70f201961e41
Luan Barbosa Morais <luannbm@gmail.com>
2024-04-17 21:06:58 -0300

    Adicionando um contador

--- olamundo.c
+++ olamundo.c
@@ -5,7 +5,14 @@ int main()
 {
     system("cls");
 
+    int numero = 0;
+
     printf("Ola mundo!\n");
+
+    while (numero < 5)
+    {
+        printf("%d\n", numero++);
+    }
 
     system("pause");
     return 0;
